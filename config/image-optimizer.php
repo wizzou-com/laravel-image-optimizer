ffed9c3d296a1ef3bf582b7235191f75638b9de5
Tim De Smet <30378686+timdesm@users.noreply.github.com>
2024-10-25 11:15:29 +0000

    Added env variables to config file

--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'default_quality' => 90,
-    'storage_disk' => 's3',  // Example: 'public', 'local', 's3'
-    'storage_path' => 'optimized_images',  // Example: 'images', 'uploads'
-    'default_format' => 'webp', // Default format for image conversion
+    'default_quality' => env('IMAGE_OPTIMIZER_QUALITY', 90),
+    'storage_disk' => env('IMAGE_OPTIMIZER_STORAGE_DISK', 's3'),  // Example: 'public', 'local', 's3'
+    'storage_path' => env('IMAGE_OPTIMIZER_STORAGE_PATH', 'optimized_images'),  // Example: 'images', 'uploads'
+    'default_format' => env('IMAGE_OPTIMIZER_FORMAT', 'webp'), // Default format for image conversion
 ];
